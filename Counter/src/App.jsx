459b2b3da8a2739dcd5d14810b6f7141d69fcb51
Aryan Chettri <aryanchettri17@gmail.com>
2025-08-13 17:08:17 +0530

    This is the first practice session upon useState. This is a counter using useState

---
+++ Counter/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(1);
+
+  const incrementCounter = () =>{
+
+    setCount(count+1);
+  }
+
+  return (
+    <>
+    <button onClick={incrementCounter}>Count</button>
+
+    <h1>{count}</h1>
+    </>
+  )
+}
+
+export default App
